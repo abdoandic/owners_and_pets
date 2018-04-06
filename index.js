fc502a1edc0c1859e9be14f399025babbface760
Adam Cooper <adam.cooper@galvanize.com>
2018-04-06 08:55:59 -0400

    Added data and route stubs

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.get('/', (request, response) => {
   response.send('Owners and Pets: Slash route is go.');
 });
 
-
+app.get('/owners', (request, response) => {});
+app.get('/owners/:id', (request, response) => {});
+app.post('/owners', (request, response) => {});
+app.put('/owners/:id', (request, response) => {});
+app.delete('/owners/:id', (request, response) => {});
 
 app.listen( PORT, () => {
   console.log(`Owners and Pets: Listening on port no. ${PORT}...`);
